dbbf3976c40d0fed770925f7ae446bbbc26bbc77
Brenda Cristaldo <112977198+brendacristaldo@users.noreply.github.com>
2024-06-17 17:29:26 -0300

    Add files via upload

---
+++ perfil/perfil-script.js
@@ -0,0 +1,11 @@
+document.querySelector('.favoritos-button').addEventListener('click', function() {
+    window.location.href = '../favoritos/favoritos.html';
+  });
+
+  document.querySelector('.editar-perfil-button').addEventListener('click', function() {
+    window.location.href = '../editar-perfil/editar.html';
+  });
+
+  document.querySelector('.receitas-button').addEventListener('click', function() {
+    window.location.href = '../minhas-receitas/minhasreceitas.html';
+  });
