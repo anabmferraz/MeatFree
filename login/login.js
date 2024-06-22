ec8930831a99c110c6ef08f3590aefbc7541ac1f
Thaisse Kirian <thakirian@gmail.com>
2024-06-22 08:55:54 -0300

    update login js

--- login/login.js
+++ login/login.js
@@ -1,5 +1,5 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Login realizado com sucesso!');
+    alert('Login realizado com sucesso!Bem-Vindo(a)!');
     window.location.href = '/index.html';
 });
